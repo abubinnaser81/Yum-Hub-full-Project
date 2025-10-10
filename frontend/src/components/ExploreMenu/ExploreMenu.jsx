6ab362ac04fedba5da02c8ec27cbf78949b51390
Abu Bin Naser <abubinnaser81@users.noreply.github.com>
2025-10-10 23:35:25 +0600

    eto vhul hsce matha onk grom hye jasce dhurr baal

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,35 +1,25 @@
 import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
-
-const ExploreMenu = ({ category, setCategory }) => {
+const ExploreMenu = ({category,setCategory}) => {
   return (
-    <div className="explore-menu" id="explore-menu">
-      <h1>Explore our menu</h1>
-
-      <p className="explore-menu-text">
-        Choose from a diverse menu featuring a delectable array of dishes crafted 
-        with the finest ingredients and culinary expertise. Our mission is to 
-        satisfy your cravings and deliver an unforgettable dining experience right 
-        to your doorstep.
-      </p>
-
-      <hr />
-
-      <div className="explore-menu-list">
-        {menu_list.map((item, index) => (
-          <div
-            key={index}
-            onClick={() =>
-              setCategory(prev => (prev === item.menu_name ? "All" : item.menu_name))
-            }
-            className="explore-menu-list-item"
-          >
-            <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt={item.menu_name} />
-            <p>{item.menu_name}</p>
-          </div>
-        ))}
-      </div>
+    <div className='explore-menu' id='explore-menu'>
+        <h1>Explore our menu</h1>
+         
+        <p className='explore-menu-text'>choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise.Our mission is to satisfy your cravings and deliver an unforgettable dining experience right to your doorstep</p>
+        <hr />
+        <div className="explore-menu-list">
+            
+        {menu_list.map((item,index)=>{
+        return (
+            <div onClick ={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)}  key={index} className="explore-menu-list-item">
+                <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt={item.menu_name} />
+                <p>{item.menu_name}</p>
+            </div>
+        )
+    
+})}
+    </div>
     </div>
   )
 }
