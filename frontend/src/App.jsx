3e0fc109337d099608cad41e2b5b8e185dcb3a02
Abu Bin Naser <abubinnaser81@users.noreply.github.com>
2025-10-10 12:21:09 +0600

    explore menu only img add korsi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,17 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-
+import  Header  from './components/Header/Header'
+import './App.css'
 const App = () => {
   return (
     <div className="app">
     <Navbar/>
-<Routes>
-  < Route path='/' element={<Home/>}/>
+    <Routes>
+   <Route path='/' element={<Home/>}/>
    <Route path='/cart' element={<Cart/>}/>
    <Route path='/placeorder' element={<PlaceOrder/>}/>  
+   {/* <Route path='/header' element={<Header/>}/> */}
 </Routes>
      </div>
   )
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,10 +4,13 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-      <div className="header-contents"></div>
-      <h2>Order Your Favorite Food Here</h2>
-      <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise.</p>
-        <button>view Menu </button>
+      <div className="header-contents">
+        <h2>Order Your <br /> Favorite Food Here</h2>
+        <p>
+         Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise.Our mission is to satisfy your cravings and deliver an unforgettable dining experience right to your doorstep.
+        </p>
+        <button>View Menu</button>
+      </div>
     </div>
   )
 }
