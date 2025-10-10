a199c6c2983b6a50ba20373c93dfb18373283cf5
Abu Bin Naser <abubinnaser81@users.noreply.github.com>
2025-10-10 10:21:56 +0600

    home header file

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents"></div>
+      <h2>Order Your Favorite Food Here</h2>
+      <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise.</p>
+        <button>view Menu </button>
+    </div>
+  )
+}
+
+export default Header
