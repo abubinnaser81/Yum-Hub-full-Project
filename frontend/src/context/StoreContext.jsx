31074190be7496a269fe919bd74ccedb3f858862
Abu Bin Naser <abubinnaser81@users.noreply.github.com>
2025-10-12 22:37:38 +0600

    add cart

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,15 +1,35 @@
 import {
-    createContext
+     createContext, useState
 } from 'react';
 import { food_list } from '../assets/assets';
 
 export const StoreContext = createContext(null);
 
 export const StoreContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState([]);
+
+
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+        }
+        else {
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+        } 
+    }
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({...prev,[itemId]:prev[itemId]-1}))
+    }
+
 
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        addToCart,
+        removeFromCart
+        // addToCart
     };
+
     // Add your context values
     return (
         <StoreContext.Provider value={contextValue}>
