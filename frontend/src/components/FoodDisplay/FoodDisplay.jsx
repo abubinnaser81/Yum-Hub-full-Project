2ec5fdc3f588438a435176c5b9a2b953e055454a
Abu Bin Naser <abubinnaser81@users.noreply.github.com>
2025-10-14 22:37:47 +0600

    objective okey

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -9,17 +9,19 @@ const FoodDisplay = ({ category }) => {
     <div className='food-display' id='food-display'>
       <h2>Top Dishes near you</h2>
       <div className="food-display-list">
-        {food_list.map((item, index) => 
-            (
-             <FoodItem key={index}
-               id={item.id}
-               name={item.name}
+        {food_list.map((item,index) => {
+          if (category === 'All' || item.category === category) {
+            return (
+              <FoodItem key={item.id || index}
+                id={item.id || index}
+                name={item.name}
                price={item.price}
                description={item.description}
                image={item.image}
                 />
            )
-        )}
+          }
+        })}
       </div>
     </div>
   )
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,12 +1,12 @@
 import {
-     createContext, useState
+     createContext, useEffect, useState
 } from 'react';
 import { food_list } from '../assets/assets';
 
 export const StoreContext = createContext(null);
 
 export const StoreContextProvider = (props) => {
-    const [cartItems, setCartItems] = useState([]);
+    const [cartItems, setCartItems] = useState({});
 
 
     const addToCart = (itemId) => {
@@ -21,13 +21,17 @@ export const StoreContextProvider = (props) => {
         setCartItems((prev) => ({...prev,[itemId]:prev[itemId]-1}))
     }
 
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems]);
 
     const contextValue = {
         food_list,
         cartItems,
+        setCartItems,
         addToCart,
         removeFromCart
-        // addToCart
+        
     };
 
     // Add your context values
