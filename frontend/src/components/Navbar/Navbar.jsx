a218786392f5fc6e0ee6df94dfe1ff08bbbd75f3
Abu Bin Naser <abubinnaser81@users.noreply.github.com>
2025-10-19 11:06:04 +0600

    cart button

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -8,23 +8,20 @@ const Navbar = ({setShowLogin}) => {
 
   return (
     <div className='navbar'>
-      {/* Logo */}
-      <img src={assets.logo_yum} alt="Yum Hub Logo" className="logo" />
-
+      
+     <Link to='/'><img src={assets.logo_yum} alt="" className="logo" /></Link> 
       {/* Menu Items */}
       <ul className="navbar-menu">
-        <Link  to='/' className={menu === "Home" ? "active" : ""} onClick={() => setMenu("Home")}>Home</Link>
-        <a href='#explore-menu' className={menu === "Menu" ? "active" : ""} onClick={() => setMenu("Menu")}>Menu</a>
-        <a href='#app-download' className={menu === "Mobaile App" ? "active" : ""} onClick={() => setMenu("Mobaile App")}>Mobaile App</a>
-        <a href='#footer' className={menu === "Contact us" ? "active" : ""} onClick={() => setMenu("Contact us")}>Contact us</a>
+        <Link  to='/' onClick={() => setMenu("home")}  className={menu === "home" ? "active" : ""} >home</Link>
+        <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""} >menu</a>
+        <a href='#app-download'  onClick={() => setMenu("mobaile App")} className={menu === "mobaile App" ? "active" : ""} >mobaile App</a>
+        <a href='#footer' onClick={() => setMenu("contact us")} className={menu === "contact us" ? "active" : ""} >contact us</a>
       </ul>
-
       {/* Right Section */}
       <div className="navbar-right">
-        <img src={assets.search_icon} alt="Search" className="navbar-icon" />
-
+        <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="Cart" className="navbar-icon" />
+         <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link> 
           <div className="dot"></div>
         </div>
 
