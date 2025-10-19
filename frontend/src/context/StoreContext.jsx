90898abd148ef2a69901e5dd8fc5e3c9d10bf42d
Abu Bin Naser <abubinnaser81@users.noreply.github.com>
2025-10-20 00:37:51 +0600

    getTotalCartAmount

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,5 +1,5 @@
 import {
-     createContext, useEffect, useState
+    createContext, useState
 } from 'react';
 import { food_list } from '../assets/assets';
 
@@ -15,23 +15,33 @@ export const StoreContextProvider = (props) => {
         }
         else {
             setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-        } 
+        }
     }
     const removeFromCart = (itemId) => {
-        setCartItems((prev) => ({...prev,[itemId]:prev[itemId]-1}))
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
 
-    useEffect(() => {
-        console.log(cartItems);
-    }, [cartItems]);
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount += itemInfo.price* cartItems[item];
+
+            }
+
+        }
+        return totalAmount;
+    }
 
     const contextValue = {
         food_list,
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart
-        
+        removeFromCart,
+        getTotalCartAmount
+
     };
 
     // Add your context values
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react'
 import './Cart.css'
-import {StoreContext} from '../../context/StoreContext'
+import { StoreContext } from '../../context/StoreContext'
 const Cart = () => {
 
-  const {cartItems,food_list,removeFromCart}=useContext(StoreContext)
+  const { cartItems, food_list, removeFromCart,getTotalCartAmount } = useContext(StoreContext)
   return (
     <div className='cart'>
       <div className="cart-items">
@@ -18,57 +18,60 @@ const Cart = () => {
         <br />
         <hr />
         {food_list.map((item, index) => {
-          if (cartItems[item._id] > 0)
-          {
+          if (cartItems[item._id] > 0) {
             return (
               <div>
-              <div className='cart-items-title cart-items-item'>
-              <img src={item.image} alt="" />
-              <p>{item.name}</p>
-              <p>${item.price}</p>
-              <p>{cartItems[item._id]}</p>
-              <p>${item.price*cartItems[item._id]}</p>
-              <p onClick={()=> removeFromCart(item._id)} className='cross'>x</p>
-              </div>
-              <hr />
+                <div className='cart-items-title cart-items-item'>
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">x</p>
+                </div>
+                <hr />
               </div>
             )
           }
         })}
       </div>
       <div className="cart-bottom">
-        <div className='cart-total'>
-          <h2>
-            Cart Totals
-          </h2>
-        <div className="cart-total-details">
-          <p>Subtotal </p>
-          <p>{0}</p>
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount()+2}</b>
+            </div>
+           
+          </div>
+           <button>PROCEED TO CHECKOUT</button>
         </div>
-        <hr />
-        <div className="cart-total-details">
-          <p>Delivery Fee</p>
-          <p>{2}</p>
-        </div>
-        <hr />
-        <div className="cart-total-details">
-          <p>Total</p>
-          <b>{0}</b>
-        </div>
-        
-        </div>
-        <button>PROCEED TO CHECKOUT</button>
-      </div>
-      <div classname='cart-promocode'>
-        <div>
-          <p>if you have a visa code,Enter it here</p>
-          <div className="cart-promocode-input">
-            <input type="text" placeholder='promo code' />
-            <button>Submit</button>
+        <div className="cart-promocode">
+          <div>
+            <p>
+              If you have a promo code,Enter it here
+            </p>
+            <div className='cart-promocode-input'>
+              <input type="text" placeholder='promocode' />
+              <button>Submit</button>
+
+            </div>
           </div>
         </div>
       </div>
-    </div>
+     </div>
+    
   )
 }
 
