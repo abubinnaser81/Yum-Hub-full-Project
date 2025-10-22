572f9a02cb3aac92d518f0f1074490a7cd149c7f
Abu Bin Naser <abubinnaser81@users.noreply.github.com>
2025-10-23 00:27:23 +0600

    localhost:4000 api food add error khaisi

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
 
 // app config
@@ -13,6 +14,9 @@ app.use(express.json())
 app.use(cors())
 //db conection
 connectDB();
+
+//api endpoint
+app.use("/app/food",foodRouter)
 
 app.get("/",(req,res)=>{
     res.send("API Working")
